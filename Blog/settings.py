f5de3b3ccd76fcee9a737e7b7d4adff8f09a94c7
adurandt <adurandt@gmail.com>
2017-02-21 21:13:03 -0600

    4 deploy

--- Blog/settings.py
+++ Blog/settings.py
@@ -135,7 +135,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/' #Ruta relativa
-#STATIC_ROOT = os.path.join(os.getcwd(),'static')
+STATIC_ROOT = os.path.join(os.getcwd(),'static')
 STATICFILES_DIRS = [os.path.join(os.getcwd(),'static')]
 
 MEDIA_URL = '/media/'
