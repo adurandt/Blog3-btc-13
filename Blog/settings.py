997ff54b366bfe94c79da2de38a3789f3a0d14ee
adurandt <adurandt@gmail.com>
2017-02-21 21:22:37 -0600

    8 deploy

--- Blog/settings.py
+++ Blog/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os, datetime
-import dj_databse_url
+import dj_database_url
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
