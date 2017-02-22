7c6c1de631ab37a2d4f3c8e7cfbd0ff5c948bb88
adurandt <adurandt@gmail.com>
2017-02-21 21:17:46 -0600

    6 deploy

--- Blog/wsgi.py
+++ Blog/wsgi.py
@@ -9,11 +9,12 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 
 import os
 
-from whitenoise.django import DjangoWhiteNoise   #suplanta rutas de staticos 
+
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Blog.settings")
 
-app = get_wsgi_application()
+application = get_wsgi_application()
 
+from whitenoise.django import DjangoWhiteNoise   #suplanta rutas de staticos 
 application = DjangoWhiteNoise(app)
